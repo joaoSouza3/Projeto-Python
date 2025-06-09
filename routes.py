3af23e950da6723d910b111ae9d1f9d54293d6e1
unknown <joaopedrodesouza559@gmail.com>
2025-06-09 11:06:41 -0300

    Update and Delete

--- routes.py
+++ routes.py
@@ -6,7 +6,7 @@ from flask import render_template, request, redirect, url_for
 from models import db, Resenha
 from flask_login import LoginManager
 from flask_login import login_required, current_user
-from flask_login import login_user
+from flask_login import login_user, logout_user
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "cadastro"
@@ -17,12 +17,15 @@ def load_user(user_id):
     return Usuario.query.get(int(user_id))
 
 #ROTAS
+
+#TELA INICIAL
 @app.route("/")    
 def homepage():
     resenhas = Resenha.query.order_by(Resenha.idResenha.desc()).all()
     return render_template("index.html", resenhas=resenhas)
 
 
+#CRIAR RESENHA
 @app.route('/criarResenha', methods=["POST"])
 @login_required
 def criarResenha():
@@ -46,16 +49,52 @@ def criarResenha():
     return redirect("/")
 
 
+#EXCLUIR RESENHA
+@app.route("/excluir/<int:id>", methods=["Post"])
+@login_required
+def excluir(id):
+    resenha = Resenha.query.get_or_404(id)
+    if resenha.idUsuario != current_user.id:
+        return "Não autorizado", 403
+    
+    db.session.delete(resenha)
+    db.session.commit()
+    return redirect("/perfil")
+
+
+#EDITAR RESENHAS
+@app.route("/editar/<int:id>", methods=["POST"])
+@login_required
+def editarResenha(id):
+    resenha = Resenha.query.get_or_404(id)
+    if resenha.idUsuario != current_user.id:
+        return "Não autorizado", 403
+    
+    resenha.titulo = request.form.get("titulo")
+    resenha.nomeLivro = request.form.get("nomeLivro")
+    resenha.genero = request.form.get("genero")
+    resenha.desc = request.form.get("desc")
+    resenha.nota = float(request.form.get("nota"))
+
+    db.session.commit()
+    return redirect("/perfil")
+
+
+#TELA PERFIL
 @app.route("/perfil")
 @login_required
 def perfil():
     resenhas_usuario = current_user.resenha
     return render_template("perfil.html", name=current_user.name, email=current_user.email, resenhas=resenhas_usuario)
 
+
+#CADASTRO
 @app.route("/cadastro")
 def cadastro():
     return render_template("cadastro.html")
 
+
+#CADASTRAR
 @app.route("/cadastrar", methods=["POST"])
 def cadastrar():
     #data = request.get_json()
@@ -76,6 +115,8 @@ def cadastrar():
     return redirect("/")
     #return jsonify({"mensagem": "Usuário criado com sucesso"}), 201
 
+
+#LOGIN
 @app.route("/login", methods=["POST"])
 def login():
     #data = request.form.get()
@@ -89,6 +130,15 @@ def login():
     return redirect("/perfil")
 
 
+#LOGOUT
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("homepage"))
+
+
+#TELA LOGIN E CADASTRO
 @app.route("/auth")
 def auth():
     return render_template("cadastro.html")
